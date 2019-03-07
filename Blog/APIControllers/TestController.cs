abcf44785e9b5dc61d2ba88aa797621adde47c28
HeheMango <q932104843@outlook.com>
2019-03-07 22:29:55 +0800

    加入REST  API和IdentityServer授权认证

---
+++ Blog/APIControllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.APIControllers
+{
+    [Route("mango/blog/[controller]")]
+    //[Produces("application/json")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<List<string>> Hello()
+        {
+            List<string> list = new List<string>();
+            return list;
+        }
+    }
+}
